beff3d7dc31d16f2ea93336f4014178eb23b8510
Nik <roo7break@gmail.com>
2013-10-02 16:49:14 +0100

    v0.2 release - stability issues fixed(ish)

--- FireBuster/FireListener.ps1
+++ FireBuster/FireListener.ps1
@@ -4,9 +4,9 @@
 FireListener is a PowerShell script that supports FireBuster in egress testing. FireListener will allow you to
 host a listening server to which FireBuster can send packets to. 
 
-Author: Nikhil Sreekunar (@roo7break)
+Author(s): Nikhil Sreekumar (@roo7break & Nikhil Mittal (@nikhil_mitt)
 License: GNU GPL v2
-Version: v0.1
+Version: v0.2
 
 .Description
 FireListener is a PowerShell script that supports FireBuster in egress testing. FireListener will allow you to
@@ -20,61 +20,89 @@ host a listening server to which FireBuster can send packets to.
 
 .Links
 http://roo7break.co.uk
+http://labofapenetrationtester.blogspot.in
 https://www.trustedsec.com/february-2012/new-tool-release-egress-buster-find-outbound-ports/
+http://poshcode.org/542
 
-.Issues
-- Hide start-job information
-- Reduce memory consumption for more than 10 background processes
-- Keep sockets open even after data is received once.
+.Changelog:
+0.2:
+- Ctrl+C implemented and ports remain open after first data is received.
+- Thanks to Nikhil Mittal (@nikhil_mitt) for his valuable inputs.
+
+0.1 - first release
 #>
 
-Param( [Parameter(Mandatory = $False)] [String] $portrange = "5000-5002")
+Param( [Parameter(Mandatory = $False)] [String] $portrange = "100-1000")
 
 function FireListener{
 
-	$socketblock = {
-			param($port = $args[1])
-			try
-			{
-				$EndPoint = New-Object System.Net.IPEndPoint([ipaddress]::any, $port)
-				$ListenSocket = New-Object System.Net.Sockets.TCPListener $EndPoint
-				$ListenSocket.Start()
-				
-				$RecData = $ListenSocket.AcceptTCPClient()
-				$Databytes = New-Object System.Byte[] 1024
-				$Stream = $RecData.GetStream()
-				while (($i = $Stream.Read($Databytes,0,$Databytes.Length)) -ne 0)
+		$socketblock = {
+				param($port = $args[1])
+				try
 				{
-					# I am not using the client data. But if you want to just remove the comments and have a go.
-					#$EncodedText = New-Object System.Text.ASCIIEncoding
-					#$clientdata = $EncodedText.GetString($Databytes,0,$i) 
-					$clientip = $RecData.Client.RemoteEndPoint
-					Write-Output "$($clientip.Address.ToString()) connected through port $($clientip.Port.ToString())"
-				}
-				$Stream.Close()
-				$ListenSocket.Stop()
-			} catch
-				{ Write-Error $Error[0]	}
-	}
-	
-	[int] $lowport = $portrange.split("-")[0]
-    [int] $highport = $portrange.split("-")[1]
-	
-    $ErrorActionPreference = 'SilentlyContinue'
-	[int] $ports = 0
-	
-    get-job | remove-job
-    
-	for($ports=$lowport; $ports -le $highport; $ports++)
-		{
-			start-job -ScriptBlock $socketblock -ArgumentList $ports | out-null
+		
+					$EndPoint = New-Object System.Net.IPEndPoint([ipaddress]::any, $port)
+					$ListenSocket = New-Object System.Net.Sockets.TCPListener $EndPoint
+					$ListenSocket.Start()
+					
+					$RecData = $ListenSocket.AcceptTCPClient()
+					$Databytes = New-Object System.Byte[] 1024
+					$Stream = $RecData.GetStream()
+					while (($i = $Stream.Read($Databytes,0,$Databytes.Length)) -ne 0 )
+					{
+						$EncodedText = New-Object System.Text.ASCIIEncoding
+						$clientdata = $EncodedText.GetString($Databytes,0,$i)
+						$clientip = $RecData.Client.RemoteEndPoint
+						Write-Output "$($clientip.Address.ToString()) connected through port $clientdata"
+					}
+					$Stream.Close()
+					$ListenSocket.Stop()		
+				 } catch
+					{ Write-Error $Error[0]	}
 		}
-	do
-	{
-		foreach($Job in (Get-Job))
+		
+		[int] $lowport = $portrange.split("-")[0]
+		[int] $highport = $portrange.split("-")[1]
+		
+		$ErrorActionPreference = 'SilentlyContinue'
+		[int] $ports = 0
+	   
+		Get-Job | Remove-Job
+
+		for($ports=$lowport; $ports -le $highport; $ports++)
+			{
+				$job = start-job -ScriptBlock $socketblock -ArgumentList $ports
+				Write-Host "Listener on $ports is"$job.State
+			}
+		[console]::TreatControlCAsInput = $true
+		while ($true)
 		{
-			Receive-Job $Job
+			# code from http://poshcode.org/542 to capture Ctrl+C
+			# start code snip
+			if ($Host.UI.RawUI.KeyAvailable -and (3 -eq [int]$Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho").Character))
+			{
+				Write-Host "Exiting and shutting down the jobs. No turning back...." -Background DarkRed
+				Sleep 2
+				Stop-Process -Id $PID
+				break;
+			}
+			# end code snip
+			$jobs = Get-Job
+			foreach ($job1 in $jobs)
+			{ 
+				Start-Sleep -Seconds 4
+							
+				Get-Job | Receive-Job
+				
+					if ($job1.state -eq "Completed")
+					{
+						for($ports=$lowport; $ports -le $highport; $ports++)
+						{
+							$job1 = start-job -ScriptBlock $socketblock -ArgumentList $ports
+							Remove-Job -state Completed | Get-Job 
+						}
+					}
+			}
 		}
-	} while(1)
 }
 FireListener
